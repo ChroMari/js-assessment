21b3f010158a35c4886b1969ae881b87b2bcb297
chromari <chromarija@gmail.com>
2022-06-16 12:36:26 +0300

    feat: add async coding

--- app/async.js
+++ app/async.js
@@ -1,11 +1,27 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
+  /**
+   * You should understand how to use promises to handle asynchronicity.
+   *
+   */
   async: function(value) {
-
+    return new Promise(function(resolve, reject) {
+      return resolve(value);
+    });
   },
 
+  /**
+   * You should be able to retrieve data from the server and return a sorted array of names.
+   *
+   */
   manipulateRemoteData: function(url) {
-
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(datas) {
+        return datas.people.map(data => data.name).sort();
+      });
   }
 };
