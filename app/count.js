b56f22dcebfa3d591e78239f7c80234571073d9b
chromari <chromarija@gmail.com>
2022-06-16 12:07:28 +0300

    feat: add count coding

--- app/count.js
+++ app/count.js
@@ -1,7 +1,30 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
+  /**
+   * Should count from start number to end number, one per 1/10th of a second.
+   * Should provide a method to cancel the counting.
+   *
+   */
   count: function (start, end) {
+    console.log(start);
 
+    const idTimer = setTimeout(() => {
+      let count = 1;
+
+      for (let i = start + 1; i <= end; i++) {
+        setTimeout(()=>{
+          console.log(i);
+        }, 100 * count);
+
+        count++;
+      }
+    },0);
+
+    this.cancel = function() {
+      clearTimeout(idTimer);
+    };
+
+    return this;
   }
 };
