e816e437a0c90ce9ec9ee70479c073c7665dee5e
chromari <chromarija@gmail.com>
2022-06-16 12:49:59 +0300

    feat: add modules coding

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,26 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
+  /**
+   * You should be able to create a function that returns a module.
+   *
+   * @example
+   *   'hello', 'matt' => {
+   *     name: matt,
+   *     greeting: hello,
+   *     sayIt: () {
+   *       'hello, matt'
+   *     }
+   *   }
+   *
+   */
   createModule: function(str1, str2) {
-
+    return ({
+      name: str2,
+      greeting: str1,
+      sayIt: function () {
+        return `${this.greeting}, ${this.name}`;
+      }
+    });
   }
 };
