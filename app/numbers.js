905077105800d1b880cf82b58b01e1cc8d393986
chromari <chromarija@gmail.com>
2022-06-16 14:25:16 +0300

    feat: add numbers coding

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,49 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
+  /**
+   * You should be able to find the value of a given bit.
+   *
+   * @example
+   *    128, 8 => 1
+   */
   valueAtBit: function(num, bit) {
 
+    const strTwo = num.toString(2);
+    const len = strTwo.length;
+
+    return bit > len ? 0 : Number(strTwo[len - bit]);
   },
 
+  /**
+   * You should be able to return the base10 representation of a binary string.
+   *
+   * @example
+   *   '11000000' => 192
+   */
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
+  /**
+   * You should be able to convert an eight-bit number to a binary string.
+   *
+   * @example
+   *   128 => '10000000'
+   */
   convertToBinary: function(num) {
-
+    const numTwo = num.toString(2);
+    return '0'.repeat(8 - numTwo.length) + num.toString(2);
   },
 
+  /**
+   * You should be able to multiply with precision.
+   *
+   * @example
+   *   3, 0.1 => 0.3
+   */
   multiply: function(a, b) {
-
+    const strTen = (a * b).toFixed(10);
+    return Number(strTen);
   }
 };
