3c78f10b2917bc05247424dd020d76744888e20a
chromari <chromarija@gmail.com>
2022-06-16 12:40:39 +0300

    feat: add logicalOperators coding

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -1,11 +1,19 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.logicalOperatorsAnswers = {
+  /**
+   * You should be able to work with logical or.
+   *
+   */
   or: function(a, b) {
-
+    return a || b;
   },
 
+  /**
+   * You should be able to work with logical and.
+   *
+   */
   and: function(a, b) {
-
+    return a && b;
   }
 };
